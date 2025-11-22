fd52f625c0c620c74b376d773fd78c35c903e879
dvtzoe <ohmtzoe@gmail.com>
2025-11-22 23:07:39 +0700

    chore: format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +10,6 @@ export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
