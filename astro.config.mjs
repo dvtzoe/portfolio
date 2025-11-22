976df34607aafe24aa14a62fd7029a8429620166
dvtzoe <ohmtzoe@gmail.com>
2025-11-22 22:13:50 +0700

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import cloudflare from '@astrojs/cloudflare';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  integrations: [react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
